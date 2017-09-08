9bb6b2fd8fce58f8ae9b25330e7caf68a13f39d6
Frank Iriarte <jurrish@gmail.com>
2017-09-08 14:22:46 -0700

    sticky header done

--- src/component/navbar/index.js
+++ src/component/navbar/index.js
@@ -10,13 +10,44 @@ import './_nav-bar.scss';
 
 
 class Navbar extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isFixed: false
+    }
+  }
+
+  componentDidMount () {
+    this.onScroll = this.onScroll.bind(this);
+    window.addEventListener('scroll', this.onScroll);
+  }
+
+  onScroll () {
+//scrollY @ 163 = start of div
+console.log(window.scrollY);
+
+    if(window.scrollY >= 163) {
+      this.setState({ isFixed: true })
+    }
+
+    if(this.state.isFixed && window.scrollY <= 163) {
+      this.setState({ isFixed: false })
+    }
+  }
+
   preventRefresh (e) {
     e.preventDefault();
     console.log('bound to click');
   }
+
   render() {
+    
+    let createdClass = this.state.isFixed ? ' navbar-div navbar-div-fixed' : 'navbar-div';
+
     return (
-      <div className='navbar-div'>
+      <div className={ createdClass }>
         <ul>
           <li><a href='#about'>About Us</a></li>
           <li><a href='#footer'>Appointments</a></li>
