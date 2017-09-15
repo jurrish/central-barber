719221f49e16419c4bec7a2393c3992d15f7458c
Frank Iriarte <jurrish@gmail.com>
2017-09-14 20:23:08 -0700

    changed background color, tinkered with line height and readability

--- src/component/hours/index.js
+++ src/component/hours/index.js
@@ -6,11 +6,20 @@ class Hours extends React.Component {
   render() {
     return(
       <div className='hours-div'>
+        <p>Hours: </p>
         <ul>
-          <li>Tuesday-Friday: 10am-8pm</li>
+          <li>Tuesday-Friday: 10am-7pm</li>
           <li>Saturday: 8am-4pm</li>
           <li>Sunday/Monday: CLOSED </li>
         </ul>
+        <div className='address-div'>
+          <p> Address: </p>
+          <ul>
+            <li> 308 22nd Ave S. </li>
+            <li> Suite 102 </li>
+            <li> Seattle, WA </li>
+          </ul>
+        </div>
         <p>Walk-ins welcome, appointments preferred!</p>
       </div>
     )
